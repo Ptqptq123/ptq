abf0d86c86c3a449f653caaa6e2cf1b6cdbb5746
テンサイ バイ <359687373ptq@gmail.com>
2020-06-09 16:29:12 +0900

    添加项目文件。

---
+++ Models/MovieGenreViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace MvcMovie.Models
+{
+    public class MovieGenreViewModel
+    {
+        public List<teacher> Teachers { get; set; }
+        public List<Movie> Movies { get; set; }
+        public SelectList Genres { get; set; }
+        public string MovieGenre { get; set; }
+        public string SearchString { get; set; }
+    }
+}
